be61f3c62f0ee1c468a9fd30db3528c00095be15
haonv003 <haonguyennvan@gmail.com>
2023-11-07 00:47:34 +0700

    dto

--- Backend/src/main/java/com/egustore/eshop/service/CustomerService.java
+++ Backend/src/main/java/com/egustore/eshop/service/CustomerService.java
@@ -1,11 +1,18 @@
 package com.egustore.eshop.service;
 
+import com.egustore.eshop.dto.ChangePasswordDTO;
 import com.egustore.eshop.dto.CustomerDTO;
+import com.egustore.eshop.dto.ForgotPasswordDTO;
+import com.egustore.eshop.dto.ResetPasswordDTO;
 import com.egustore.eshop.model.Customer;
 
 import java.util.List;
 
 public interface CustomerService {
+    void updateProfile(int id, CustomerDTO customerDTO);
+    void changePassword(int id, ChangePasswordDTO changePasswordDTO);
+    void forgotPassword(ForgotPasswordDTO forgotPasswordDTO);
+    void resetPassword(ResetPasswordDTO resetPasswordDTO);
     Customer createCustomer(CustomerDTO customerDTO);
 
     Customer getCustomerById(int id);
