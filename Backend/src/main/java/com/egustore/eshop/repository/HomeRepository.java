3b6ad521dd40add86a9b9afa87de7d792426663d
Ngaoka <139799500+TangHuy0801@users.noreply.github.com>
2023-11-30 00:24:17 +0700

    top sale

--- Backend/src/main/java/com/egustore/eshop/repository/HomeRepository.java
+++ Backend/src/main/java/com/egustore/eshop/repository/HomeRepository.java
@@ -8,24 +8,24 @@ import java.util.List;
 
 public interface HomeRepository extends JpaRepository<Home,Integer> {
 
-    @Query(value = "    SELECT p.id as 'id',\n" +
-            "    p.model as \"Name\",\n" +
-            "    p.thumbnail as \"thumbnail\",\n" +
-            "    p.price as 'Price',\n" +
-            "    p.discount_price as 'Discount',\n" +
-            "    sum(r.Rating) as 'rate',\n" +
-            "    count(r.Rating) as 'count' from egu_store.products p\n" +
-            "    join egu_store.rating_products r\n" +
-            "    on r.product_id = p.id group by r.product_id\n" +
-            "    UNION ALL\n" +
-            "    SELECT p.id as 'id',\n" +
-            "    p.model as \"Name\",\n" +
-            "    p.thumbnail,\n" +
-            "    p.price as 'Price',\n" +
-            "    p.discount_price as 'Discount',\n" +
-            "            0 as 'rate',\n" +
-            "            0 as 'count' from egu_store.products p\n" +
-            "    Where p.id not in (SELECT product_id FROM egu_store.rating_products)", nativeQuery = true)
+    @Query(value ="SELECT p.id as id," +
+            "p.model as Name, " +
+            "p.thumbnail as thumbnail," +
+            "p.price as Price," +
+            "p.discount_price as Discount, " +
+            "sum(r.Rating) as rate, " +
+            "count(r.Rating) as count from egu_store.products p join egu_store.rating_products r " +
+            "on r.product_id = p.id " +
+            "group by r.product_id " +
+            "UNION ALL " +
+            "SELECT p.id as id, " +
+            "p.model as Name, " +
+            "p.thumbnail, " +
+            "p.price as Price , " +
+            "p.discount_price as Discount , " +
+            "0 as rate, " +
+            "0 as count from egu_store.products p Where p.id " +
+            "not in (SELECT product_id FROM egu_store.rating_products)", nativeQuery = true)
     List<Home> getAllListWithRating();
 
 }
