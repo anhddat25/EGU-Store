6e401dbccc1cf15bd8b864d33cc3e0c141a3e5a1
thanhduygreep1995 <thanhduygreep1995@gmail.com>
2023-11-30 15:09:26 +0700

    fix report
    fix report

--- Backend/src/main/java/com/egustore/eshop/repository/IncomeReportRepository.java
+++ Backend/src/main/java/com/egustore/eshop/repository/IncomeReportRepository.java
@@ -11,12 +11,24 @@ import java.util.Date;
 import java.util.List;
 
 public interface IncomeReportRepository extends JpaRepository<IncomeReport, Date> {
-    @Query(value = "CALL spSelectedByYear(:startDate, :endDate)", nativeQuery = true)
+    @Query(value = "select \n" +
+            "\tdate(o.order_date) as 'Date',\n" +
+            "\tcount(o.Id) as 'Orders', \n" +
+            "\tsum(od.Quantity*od.total_price) 'Revenue' from egu_store.orders o \n" +
+            "\tjoin egu_store.order_details od on  o.Id = od.order_id\n" +
+            "     WHERE DATE(o.order_date) BETWEEN :startDate AND :endDate AND o.status = 'DELIVERED'\n" +
+            "\tgroup by date(o.order_date)", nativeQuery = true)
     List<IncomeReport> spSelectedByYear(
             @Param("startDate") Date startDate,
             @Param("endDate") Date endDate
     );
 
-    @Query(value = "CALL spSelectedDefaultByYear()", nativeQuery = true)
+    @Query(value = "\tselect \n" +
+            "    date(o.order_date)as 'Date',\n" +
+            "\tcount(o.Id) as 'Orders', \n" +
+            "\tsum(od.Quantity*od.total_price) \"Revenue\" \n" +
+            "    from egu_store.orders o \n" +
+            "\tjoin egu_store.order_details od on  o.Id = od.order_id where o.status = 'DELIVERED'\n" +
+            "\tgroup by date(o.order_date)", nativeQuery = true)
     List<IncomeReport> spSelectedDefaultByTime();
 }
